623a99db900614ff4172cf41a71014cc667d1d8d
BillyAbrahamDeveloper <contact@billydev.me>
2024-01-20 22:41:05 -0600

    Setup Navbar and Footer

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,6 +1,21 @@
+import { getCart } from '@/lib/db/cart';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import ShoppingCartButton from './ShoppingCartButton';
 
-const Navbar = () => {
+async function searchProduct(formData: FormData) {
+  'use server';
+
+  const searchQuery = formData.get('searchQuery')?.toString();
+
+  if (searchQuery) {
+    redirect('/search?query' + searchQuery);
+  }
+}
+
+const Navbar = async () => {
+  const cart = await getCart();
+
   return (
     <div className=' bg-base-100'>
       <div className='navbar max-w-7xl m-auto flex-col sm:flex-row gap-2'>
@@ -10,6 +25,20 @@ const Navbar = () => {
               Shopbify
             </h1>
           </Link>
+        </div>
+        <div className='flex-none gap-2'>
+          <form action={searchProduct}>
+            {' '}
+            <div className='form-control'>
+              <input
+                type='text'
+                name='searchQuery'
+                placeholder='Seach'
+                className='input input-bordered w-full max-w-[200px]'
+              />
+            </div>
+          </form>
+          <ShoppingCartButton cart={cart} />
         </div>
       </div>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from './Navbar/Navbar';
+import Footer from './Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,9 +20,10 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Navbar />
-        <main className=' p-4 max-w-7xl min-w-[300px] m-auto  '>
+        <main className=' min-h-[500px] flex flex-col justify-center items-center p-4 max-w-7xl min-w-[300px] m-auto  '>
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
